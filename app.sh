bc64c0ea6ef7d55c00c0caacf6ec315af79673bc
david.hendri <david@solvo.cloud>
2023-04-23 21:35:19 +0300

    wip

--- app.sh
+++ app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the secret name from the first command line argument
-secret_name="$1"
+secret_name=$APP_SECRET_NAME
 
 # Use the AWS CLI to get the value of the secret
 secret_value=$(aws secretsmanager get-secret-value --secret-id $secret_name --query SecretString --output text)
@@ -9,7 +9,7 @@ secret_value=$(aws secretsmanager get-secret-value --secret-id $secret_name --qu
 echo "The value of $secret_name is: $secret_value"
 
 # Replace <bucket-name> and <object-key> with the S3 bucket and object key of the file you want to download
-bucket_name="$2"
+bucket_name=$APP_BUCKET_NAME
 object_key="tenant/54ab556f-9270-4057-9f9b-e28c7eac0ea2/user/2f01d261-0f9c-4e5d-b8b3-b3926c4c2b3c/data.json"
 output_file="downloaded_file.txt"
 
@@ -19,7 +19,7 @@ aws s3 cp s3://$bucket_name/$object_key $output_file
 echo "File downloaded to $output_file"
 
 # Replace <table-name> with the name of your DynamoDB table
-table_name="$3"
+table_name=$APP_TABLE_NAME
 key_name="id"
 key_value="123"
 
